72154017effd7fb2cd2582e5437837a40aa019e8
chintan712 <chintandarji155@gmail.com>
2026-09-20 22:32:31 -0400

    changed cors allowing domains for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,10 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(title=settings.app_name, version="0.1.0", lifespan=lifespan)
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173"], allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173",
+                                                "http://127.0.0.1:5174",
+                                                "https://investment-research-copilot-nkp4lh8k9-dchintan2204-7335.vercel.app",
+], allow_methods=["*"], allow_headers=["*"])
 app.include_router(documents_router)
 app.include_router(chat_router)
 app.include_router(requests_router)
